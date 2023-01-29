5fd9c2fa95c11ca1d6be0a4953f6f9d424da9fcd
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-29 13:52:10 +0000

    touched up layout and added line that will feature in scroll animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ import {motion} from "framer-motion"
 
 export const Landing = () => {
   return (
-    <motion.div className="landing-section">          
+    <motion.div className="landing-section">  
       <motion.div className="banner-container ">         
         <h1 className="banner">WE BUILD INCREDIBLE COMPACT KITCHEN SOLUTIONS</h1>
       </motion.div>
@@ -13,6 +13,9 @@ export const Landing = () => {
       <motion.div className="landing-message-container">
         <h4 className="landing-message">An innovative, design company, creating and building customised kitchen units</h4>
       </motion.div>
+      <div className="landing-bottom-line">
+        <span></span>
+      </div>
     </motion.div>
   )
 }
