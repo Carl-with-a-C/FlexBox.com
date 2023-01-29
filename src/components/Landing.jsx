d93fec6e0b7555d367860f9e0f2799eebfe3817e
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-29 22:33:00 +0000

    added layout for about section

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,9 +13,7 @@ export const Landing = () => {
       <motion.div className="landing-message-container">
         <h4 className="landing-message">An innovative, design company, creating and building customised kitchen units</h4>
       </motion.div>
-      <div className="landing-bottom-line">
-        <span></span>
-      </div>
+      <motion.div className="landing-bottom-line"></motion.div>
     </motion.div>
   )
 }
