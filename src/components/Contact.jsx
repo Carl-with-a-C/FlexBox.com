eb9913cd55f21819f78db4c75af03280e0f4f959
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-30 16:11:44 +0000

    contact page added with image and contact info

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+
+export const Contact = () => {
+  return (
+    <motion.div className="contact-section">
+      <motion.h2 className="contact-section--header">Get in touch.</motion.h2>
+      <div className="">
+        <img
+          className="contact-section--img"
+          src={process.env.PUBLIC_URL + "./images/contact.jpg"}
+          alt="candle-lit meal"
+        />
+      </div>
+      <div className="contact-section--info-container">
+        <p>General Enquiries</p>
+        <a>info@flexbox.com</a>
+      </div>
+    </motion.div>
+  );
+};
