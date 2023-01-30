6c4e3a55669dbd13361073e6fcace8718c76b6ca
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-30 16:56:42 +0000

    animation on landing header tested

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,19 +1,92 @@
-import React from 'react'
+import React from "react";
 
-import {motion} from "framer-motion"
+import { motion, transform } from "framer-motion";
 
 export const Landing = () => {
-  return (
-    <motion.div className="landing-section">  
-      <motion.div className="banner-container ">         
-        <h1 className="banner">WE BUILD INCREDIBLE COMPACT KITCHEN SOLUTIONS</h1>
-      </motion.div>
-   
+  const line1 = "WE BUILD";
+  const line2 = "INCREDIBLE";
+  const line3 = "COMPACT";
+  const line4 = "KITCHEN SOLUTIONS";
 
-      <motion.div className="landing-message-container">
-        <h4 className="landing-message">An innovative, design company, creating and building customised kitchen units</h4>
+  const sentence = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+        staggerChildren: 0.08,
+      },
+    },
+  };
+
+  const letter = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  return (
+    <motion.div className="landing-section">
+      <motion.h1
+        className="banner-container"
+        variants={sentence}
+        initial="hidden"
+        animate="visible"
+      >
+        {line1.split("").map((char, index) => {
+          return (
+            <motion.span key={`${char} ${index}`} variants={letter}>
+              {char}
+            </motion.span>
+          );
+        })}
+        <br />
+
+        {line2.split("").map((char, index) => {
+          return (
+            <motion.span key={`${char} ${index}`} variants={letter}>
+              {char}
+            </motion.span>
+          );
+        })}
+        <br />
+
+        {line3.split("").map((char, index) => {
+          return (
+            <motion.span key={`${char} ${index}`} variants={letter}>
+              {char}
+            </motion.span>
+          );
+        })}
+        <br />
+
+        {line4.split("").map((char, index) => {
+          return (
+            <motion.span key={`${char} ${index}`} variants={letter}>
+              {char}
+            </motion.span>
+          );
+        })}
+      </motion.h1>
+
+      <motion.div
+        initial={{ opacity: 0, y: 60 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 0.8,
+          delay: 3,
+        }}
+        className="landing-message-container"
+      >
+        <h4 className="landing-message">
+          An innovative, design company, creating and building customised
+          kitchen units
+        </h4>
       </motion.div>
       <motion.div className="landing-bottom-line"></motion.div>
     </motion.div>
-  )
-}
+  );
+};
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import {motion} from "framer-motion"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export const Nav = () => {
   return (
-    <motion.div     
-    initial={{ opacity: 0, y: -180 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{
-      ease: "easeInOut",
-      duration: 1,
-      delay: 0.6,
-    }} 
-    className="nav-container nav-section">
+    <motion.div
+      initial={{ opacity: 0, y: -180 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 2,
+        delay: 2,
+      }}
+      className="nav-container nav-section"
+    >
       <div className="nav-logo-container">
-        <div className='logo'>
+        <div className="logo">
           <a>flexbox</a>
         </div>
       </div>
-        <div className='hamburger-menu'>
-          <span></span>
-        </div>
+      <div className="hamburger-menu">
+        <span></span>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
